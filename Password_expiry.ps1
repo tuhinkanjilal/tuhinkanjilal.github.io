eef15c1cd831435a2d31646ba09a0743cce62010
tuhinkanjilal <66356080+tuhinkanjilal@users.noreply.github.com>
2026-08-26 16:23:56 +0530

    Add files via upload

---
+++ Password_expiry.ps1
@@ -0,0 +1,128 @@
+##################################################################################################################
+$smtpServer="smtp.channel4.co.uk"
+$expireindays = 21
+$from = "Password_Notification <password-notification@channel4.co.uk>"
+$logging = "disabled" # Set to Disabled to Disable Logging
+$logFile = "E:\pwdexp.txt" # ie. c:\mylog.csv
+$testing = "Disabled" # Set to Disabled to Email Users
+#$testRecipient = "tkanjilal@channel4.co.uk","jwarri@channel4.co.uk"
+#
+###################################################################################################################
+
+# Check Logging Settings
+if (($logging) -eq "Enabled")
+{
+    # Test Log File Path
+    $logfilePath = (Test-Path $logFile)
+    if (($logFilePath) -ne "True")
+    {
+        # Create CSV File and Headers
+        New-Item $logfile -ItemType File
+        Add-Content $logfile "Date,Name,EmailAddress,DaystoExpire,ExpiresOn,Notified"
+    }
+} # End Logging Check
+
+# System Settings
+$textEncoding = [System.Text.Encoding]::UTF8
+$date = Get-Date -format ddMMyyyy
+# End System Settings
+
+# Get Users From AD who are Enabled, Passwords Expire and are Not Currently Expired
+Import-Module ActiveDirectory
+#$users = get-aduser -identity tkanjilal -properties Name, PasswordNeverExpires, PasswordExpired, PasswordLastSet, EmailAddress |where {$_.Enabled -eq "True"} | where { $_.PasswordNeverExpires -eq $false } | where { $_.passwordexpired -eq $false }
+$users = get-aduser -filter * -properties Name, PasswordNeverExpires, PasswordExpired, PasswordLastSet, EmailAddress |where {$_.Enabled -eq "True"} | where { $_.PasswordNeverExpires -eq $false } | where { $_.passwordexpired -eq $false }
+$DefaultmaxPasswordAge = (Get-ADDefaultDomainPasswordPolicy).MaxPasswordAge
+
+# Process Each User for Password Expiry
+foreach ($user in $users)
+{
+    $Name = $user.Name
+    $emailaddress = $user.emailaddress
+    $passwordSetDate = $user.PasswordLastSet
+    $PasswordPol = (Get-AduserResultantPasswordPolicy $user)
+    $sent = "" # Reset Sent Flag
+    # Check for Fine Grained Password
+    if (($PasswordPol) -ne $null)
+    {
+        $maxPasswordAge = ($PasswordPol).MaxPasswordAge
+    }
+    else
+    {
+        # No FGP set to Domain Default
+        $maxPasswordAge = $DefaultmaxPasswordAge
+    }
+
+
+    $expireson = $passwordsetdate + $maxPasswordAge
+    $today = (get-date)
+    $daystoexpire = (New-TimeSpan -Start $today -End $Expireson).Days
+
+    # Set Greeting based on Number of Days to Expiry.
+
+    # Check Number of Days to Expiry
+    $messageDays = $daystoexpire
+
+    if (($messageDays) -gt "1")
+    {
+        $messageDays = "in " + "$daystoexpire" + " days."
+    }
+    else
+    {
+        $messageDays = "today."
+    }
+
+    # Email Subject Set Here
+    $subject="Your password will expire $messageDays"
+
+    # Email Body Set Here, Note You can use HTML, including Images.
+    $body ="
+    Dear $name,
+    <p> Your Password will expire $messageDays You must change it as soon as possible.<br>
+    To change your password on a PC press CTRL+ALT+Delete and choose Change Password <br>
+    Please do not respond to this message as this is an automated alert.
+    <p>Kind Regards, <br>
+    IT-Service helpdesk <br>
+    </P>"
+
+
+    # If Testing Is Enabled - Email Administrator
+    if (($testing) -eq "Enabled")
+    {
+        $emailaddress = $testRecipient
+    } # End Testing
+
+    # If a user has no email address listed
+    if (($emailaddress) -eq $null)
+    {
+        $emailaddress = $testRecipient
+    }# End No Valid Email
+
+    # Send Email Message
+  #  if (($daystoexpire -ge "0") -and ($daystoexpire -lt $expireindays))
+ if (($daystoexpire -ge "0") -and ($daystoexpire -lt $expireindays) -and ($daystoexpire -eq "21") -or ($daystoexpire -eq "15") -or ($daystoexpire -eq "7")  -or ($daystoexpire -le "0" ))
+    {
+        $sent = "Yes"
+        # If Logging is Enabled Log Details
+        if (($logging) -eq "Enabled")
+        {
+            Add-Content $logfile "$date,$Name,$emailaddress,$daystoExpire,$expireson,$sent"
+        }
+        # Send Email Message
+        Send-Mailmessage -smtpServer $smtpServer -from $from -to $emailaddress -subject $subject -body $body -bodyasHTML -priority High -Encoding $textEncoding
+
+    } # End Send Message
+    else # Log Non Expiring Password
+    {
+        $sent = "No"
+        # If Logging is Enabled Log Details
+        if (($logging) -eq "Enabled")
+        {
+            Add-Content $logfile "$date,$Name,$emailaddress,$daystoExpire,$expireson,$sent"
+        }
+    }
+
+} # End User Processing
+
+
+
+# End
